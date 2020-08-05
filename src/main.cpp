690514b4f49011c91ef865f74f3bf45eae05f077
Anton Karmanov <bergertroll@insiberia.net>
2020-08-05 19:54:01 +0500

    Add pseudocode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <cstdlib>
+#include <string>
 
 int main(int argc, char **argv) {
+  size_t mThreadsNum { 6 };
+  size_t rThreadsNum { 6 };
+  string filename { "big_set.txt" }
+
+  auto [blockSize, entries] { splitFile(filename, mThreadsNum };
+
+  ThreadPool mapThreads { };
+
+  for (auto entry: entries) {
+    mapThread.run(Mapper(entry, blockSize));
+  }
+
+  mapThreads.join();
+
+  ThreadPool shuffleThreads { } 
+
+  // TODO run shuffle
+
+  shuffleThreads.join();
+
+  ThreadPool reduceThreads { };
+
+  // TODO run reduce
+
+  reduceThreads.join();
+
   return EXIT_SUCCESS;
 }
