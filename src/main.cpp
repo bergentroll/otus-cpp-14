888f3fe00a29044da9980a639135f86dc6189ebd
Anton Karmanov <bergertroll@insiberia.net>
2020-08-06 23:29:01 +0500

    Add program options

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include <boost/program_options.hpp>
+#include <boost/serialization/strong_typedef.hpp>
 #include <cstdlib>
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "thread_pool.hpp"
@@ -8,11 +10,66 @@
 
 using namespace std;
 using namespace otus;
+namespace po = boost::program_options;
+using 
+  boost::any,
+  boost::any_cast,
+  boost::lexical_cast,
+  boost::bad_lexical_cast;
+
+BOOST_STRONG_TYPEDEF(int, ThreadsNum);
+
+void validate(any& v, const vector<string>& values, ThreadsNum*, int) {
+  po::validators::check_first_occurrence(v);
+  const string &str { po::validators::get_single_string(values) };
+  try {
+    v = lexical_cast<ThreadsNum>(str);
+  } catch (bad_lexical_cast const &e) {
+    throw po::error("unexpected threads amount " + str);
+  }
+  auto port { any_cast<ThreadsNum>(v) };
+  if (port < 1) throw po::error("threads amount must be greater than 0");
+}
 
 int main(int argc, char **argv) {
-  size_t mapThreadsNum { 30 };
-  size_t reduceThreadsNum { 6 };
-  string filename { "../small_steady_set.txt" };
+  string filename;
+  size_t mapThreadsNum;
+  size_t reduceThreadsNum;
+
+  try {
+    po::positional_options_description positional_description { };
+    positional_description.add("src", 1);
+    positional_description.add("mnum", 1);
+    positional_description.add("rnum", 1);
+
+    po::options_description description { "Hidden options" };
+    description.add_options()
+      ("src", po::value<string>()->composing()->required(),
+       "input file")
+      ("mnum", po::value<ThreadsNum>()->composing()->required(),
+       "numer of mapping threads")
+      ("rnum", po::value<ThreadsNum>()->composing()->required(),
+       "number of reduce threads");
+
+    po::variables_map options;
+
+    po::store(
+      po::command_line_parser(argc, argv)
+        .positional(positional_description)
+        .options(description)
+        .run(),
+      options);
+    po::notify(options);
+
+    filename = options["src"].as<string>();
+    mapThreadsNum = options["mnum"].as<ThreadsNum>();
+    reduceThreadsNum = options["rnum"].as<ThreadsNum>();
+
+    cerr << "OLOLO has been set to " << mapThreadsNum << endl;
+  } catch (po::error const &e) {
+    cerr << "Options error: " << e.what() << endl;
+    return EXIT_FAILURE;
+  }
 
   std::vector<FileMarker::PosType> marks { };
   marks.reserve(mapThreadsNum);
