2906af1a7d4b40d74bff83d9e9c98b9885847b23
Anton Karmanov <bergertroll@insiberia.net>
2020-08-10 18:56:19 +0500

    Add Mapper logic for prefix mapping

--- inc/mapper.hpp
+++ inc/mapper.hpp
@@ -11,6 +11,8 @@
 namespace otus {
   class Mapper {
   public:
+    using MappingResult = std::vector<std::string>;
+
     Mapper(std::string const &filename, PosType begin, PosType end):
     file(filename) {
       // TODO Errors handling.
@@ -19,17 +21,33 @@ namespace otus {
       while (file.tellg() <= end && std::getline(file, line)) {
         tokens.push_back(line);
       }
+      result.reserve(tokens.size());
     }
 
     void run() {
-      std::string s { };
-      file >> s;
-      //std::cerr << s << std::endl;
+      for (auto const &token: tokens)
+        result.push_back(process(token));
     }
+
+    std::vector<MappingResult>& getResult() { return result; }
 
   private:
     std::ifstream file;
     // TODO Reserve.
     std::vector<std::string> tokens { };
+    std::vector<MappingResult> result { };
+
+    static inline MappingResult process(std::string const &item) {
+      MappingResult result { };
+      result.reserve(item.length());
+
+      std::string buf { };
+      for (auto ch: item) {
+        buf += ch;
+        result.push_back(buf);
+      }
+
+      return result;
+    }
   };
 }
