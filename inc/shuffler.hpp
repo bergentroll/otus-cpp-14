7351e4e25879abae25abfc590a2fbd9e386d5d10
Anton Karmanov <bergertroll@insiberia.net>
2020-08-13 23:26:33 +0500

    Update Shuffler

--- inc/shuffler.hpp
+++ inc/shuffler.hpp
@@ -23,23 +23,27 @@ namespace otus {
       result.reserve(itemsAmount);
     };
 
+    /// FIXME Still errors in sorting.
     void operator()() {
-      while (!isFinished()) {
+      while (true) {
 
-        auto [targetContainerIndex, prevMin] { getTarget() };
+        auto [targetContainerIndex, secondMin] { getTarget() };
 
         auto const &target { input[targetContainerIndex] };
         auto &it { iterators[targetContainerIndex] };
+
+        if (containersRemain == 0) break;
+        else if (containersRemain == 1) {
+          secondMin = target.back().first;
+        }
 
         /// FIXME Use upper_bound for efficient.
         auto end { std::find_if(
             target.cbegin(),
             target.cend(),
-            [&prevMin](auto const &item) { return item.first > prevMin; }) };
+            [&secondMin](auto const &item) { return item.first > secondMin; }) };
 
         std::copy(it, end, std::back_inserter(result));
-
-        if (end == target.cend()) std::cerr << "End of " << targetContainerIndex << " reached\n";
 
         it = end;
       }
@@ -51,36 +55,32 @@ namespace otus {
     InputType &input;
     std::vector<ItemType> result { };
     std::vector<std::vector<ItemType>::const_iterator> iterators { };
-
-    bool isFinished() {
-      for (size_t i { }; i < input.size(); ++i) {
-        if (iterators[i] != input[i].cend()) {
-          std::cerr << "Not finished: " << i << ", value: " << (*iterators[i]).first << std::endl;
-          return false;
-        }
-      }
-      return true;
-    }
+    size_t containersRemain { };
 
     std::pair<size_t, std::string> getTarget() {
-      std::string min { "Zzz" }, prevMin { "Zzz" }; // FIXME
+      std::string min { }, secondMin { };
       size_t targetContainerIndex { };
+
+      containersRemain = 0;
 
       for (size_t i { }; i < input.size(); ++i) {
         auto it { iterators[i] };
         if (it == input[i].cend()) continue;
 
+        ++containersRemain;
+
         auto const &value { (*it).first };
-        if (value < min) {
-          prevMin = min;
+        if (value < min || min.empty()) {
           min = value;
           targetContainerIndex = i;
         }
+
+        if ((value < secondMin && i != targetContainerIndex) || secondMin.empty()) {
+          secondMin = value;
+        }
       }
 
-      std::cerr << "ContIndex: " << targetContainerIndex
-        << ", prevMin: " << prevMin << ", min: " << min << "\n";
-      return std::make_pair(targetContainerIndex, prevMin);
+      return std::make_pair(targetContainerIndex, secondMin);
     }
   };
 }
